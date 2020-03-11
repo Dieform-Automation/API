7a7d959c0b79adcee68e45adf847afb55239538b
Amar Jasarbasic <amarjasarbasic@gmail.com>
2020-03-11 17:03:28 -0400

    added part-order association

--- manage.py
+++ manage.py
@@ -6,7 +6,7 @@ from flask_script import Manager
 
 from app import blueprint
 from app.main import create_app, db
-from app.main.model import user, blacklist, customer, part, order
+from app.main.model import user, blacklist, customer, part, order, part_order
 
 app = create_app(os.getenv('ENVIRON') or 'dev')
 app.register_blueprint(blueprint)
